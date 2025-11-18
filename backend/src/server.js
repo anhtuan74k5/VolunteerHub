d902eef11c0527f2a010b371b06aa6e96ab7e3db
daond <160707743+daoendi@users.noreply.github.com>
2025-11-18 23:00:59 +0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -62,7 +62,6 @@ app.use("/api/dashboard", dashboardRoutes); // Routes cho dashboard
 app.use("/api/statistics", statisticsRoutes); // Routes cho thống kê
 app.use("/api/notifications", notificationRoutes); // Routes cho thông báo
 // Tạo route ảo /uploads để trỏ vào thư mục /uploads thật
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 // =================================================================================================
 // Xử lý lỗi 404 - Route không tồn tại
